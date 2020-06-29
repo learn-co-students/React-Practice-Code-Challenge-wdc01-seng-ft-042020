81d200ea6fbc09180badfd72cc1903a1f66574c6
jlangley3 <jlangleyiii@aol.com>
2020-06-29 12:03:21 -0400

    Done.

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -4,14 +4,14 @@ const Table = (props) => {
 
   const renderPlates = (array) => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div key={index} className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
 
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${ props.remainingBudget } remaining!
       </h1>
       <div className="table">
         <div className="stack">
